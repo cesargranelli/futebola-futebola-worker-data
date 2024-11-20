944d1a0a119648085c1e7e4c4a94a0441f1167da
Cesar Granelli <cesar.granelli@gmail.com>
2024-11-19 22:19:53 -0300

    first commit

---
+++ src/test/java/com/futebola/worker/FutebolaWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futebola.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutebolaWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
